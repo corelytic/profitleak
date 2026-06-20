a12a7ae93effbc169acd86e8e9916b0b1300ec73
corelytic <omnilabs76@gmail.com>
2026-06-20 11:58:59 +0300

    Add files via upload

---
+++ assets/js/demo.js
@@ -0,0 +1,9 @@
+(function(){
+  var links = document.querySelectorAll('a[href^="#"]');
+  links.forEach(function(link){
+    link.addEventListener('click', function(event){
+      var target = document.querySelector(link.getAttribute('href'));
+      if(target){ event.preventDefault(); target.scrollIntoView({behavior:'smooth'}); }
+    });
+  });
+})();
