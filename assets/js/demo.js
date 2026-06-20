257b5da73644bba2d2be359342f640b911acef20
corelytic <omnilabs76@gmail.com>
2026-06-20 11:48:36 +0300

    Delete assets directory

--- assets/js/demo.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  var links = document.querySelectorAll('a[href^="#"]');
-  links.forEach(function(link){
-    link.addEventListener('click', function(event){
-      var target = document.querySelector(link.getAttribute('href'));
-      if(target){ event.preventDefault(); target.scrollIntoView({behavior:'smooth'}); }
-    });
-  });
-})();
